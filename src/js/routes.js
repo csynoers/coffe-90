8001432c79ca1fc02ded05aa5622a6b90ec48886
csynoers <3s0c9m7@gmail.com>
2021-02-06 21:43:36 +0700

    tes app

--- src/js/routes.js
+++ src/js/routes.js
@@ -59,14 +59,14 @@ import DaftarAntrianPage from '../pages/daftar-antrian.f7.html';
 import NotFoundPage from '../pages/404.f7.html';
 
 var routes = [
-  {
-    path: '/',
-    component: LoginPage,
-  },
   // {
   //   path: '/',
-  //   component: DaftarAntrianPage,
+  //   component: LoginPage,
   // },
+  {
+    path: '/',
+    component: HomeAdminPage,
+  },
   // {
   //   path: '/',
   //   component: LaporanStokPage,
