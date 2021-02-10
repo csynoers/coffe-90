9f7f2595bc16ff152abb5fb339e01ad31df526c4
csynoers <3s0c9m7@gmail.com>
2021-02-10 21:30:22 +0700

    fix data users

--- src/js/routes.js
+++ src/js/routes.js
@@ -30,13 +30,9 @@ import DaftarAntrianPage from '../pages/daftar-antrian.f7.html';
 import NotFoundPage from '../pages/404.f7.html';
 
 var routes = [
-  // {
-  //   path: '/',
-  //   component: LoginPage,
-  // },
   {
     path: '/',
-    component: UsersPage,
+    component: LoginPage,
   },
   {
     path: '/login/',
